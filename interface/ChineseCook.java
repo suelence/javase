075600cf0fe0135b4c7eb53b0373ae9d52b6a6fc
accelerator <1945495231@qq.com>
2021-05-24 14:08:31 +0800

    master

--- interface/ChineseCook.java
+++
@@ -1,8 +0,0 @@
-public class ChineseCook implements FoodMenu  {
-    public void scrambleEggsTomatoes(){
-        System.out.println("中餐西红寺炒鸡蛋");
-    } 
-    public void yuXiangRouSi(){
-        System.out.println("中餐鱼香肉丝");
-    }
-}
--- interface/WesternCook.java
+++
@@ -1,8 +0,0 @@
-public class WesternCook implements FoodMenu {
-    public void scrambleEggsTomatoes(){
-        System.out.println("西餐西红寺炒鸡蛋");
-    }
-    public void yuXiangRouSi(){
-        System.out.println("西餐鱼香肉丝");
-    }
-}
