f9c557e86ffd57cf10bf4d3a12a57d1773a90b6a
accelerator <1945495231@qq.com>
2021-05-28 17:06:25 +0800

    master

--- array/MyStackTest.java
+++ array/MyStackTest.java
@@ -2,7 +2,6 @@ public class MyStackTest {
     public static void main(String[] args){
         MyStack stack = new MyStack();
         stack.push(new Object());
-        stack.pop();
-        
+        stack.pop();       
     }
 }
